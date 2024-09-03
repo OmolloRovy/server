e6d99a681cc862fb633d52fef778d91f5b01450f
omolloRovy <masimevanistelrooy@gmail.com>
2024-09-03 12:39:45 +0300

    login limiter

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -18,6 +18,16 @@ const logEvents = async (message, logFileName) => {
     }
 }
 
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 login requests per windowMs
+    message: { message: "Too many login attempts from this IP, please try again after 15 minutes." },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const logger = (req, res, next) => {
     logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
     console.log(`${req.method} ${req.path}`)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-const loginLimiter = require('../middleware/loginLimiter')
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const loginLimiter = require('../middleware/loginLimiter');
 
 router.route('/')
-    .post(loginLimiter, authController.login)
+    .post(loginLimiter, authController.login);  
 
 router.route('/refresh')
-    .get(authController.refresh)
+    .get(authController.refresh);
 
 router.route('/logout')
-    .post(authController.logout)
+    .post(authController.logout);
 
-module.exports = router
+module.exports = router;
